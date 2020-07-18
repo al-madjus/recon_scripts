6378c28085428b380688170be1738a459794e3e6
Al-Madjus <klarsen@klarsen.net>
2020-07-18 18:47:16 +0200

    Adjusted masscan rate

--- portscan.sh
+++ portscan.sh
@@ -5,4 +5,4 @@ DIR=$1/scope
 cat $DIR/ip_hosts.txt | jq '.data.answers' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u > $DIR/ip.txt
 
 ### Run masscan ### 
-sudo masscan -iL $DIR/ip.txt -p0-1000 -oX $DIR/masscan.txt
+sudo masscan -iL $DIR/ip.txt -p0-1000 --rate 500 -oX $DIR/masscan.txt
