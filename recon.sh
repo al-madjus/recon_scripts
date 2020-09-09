0cbecc065adb42f991d1a358fa74e6368f060f8a
Al-Madjus <klarsen@klarsen.net>
2020-09-09 12:14:41 +0200

    Expanded paths

--- recon.sh
+++ recon.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Set PATH here since cron has very limited PATH
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/loca/sbin
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-while read p; do ~/Pentesting/recon/recurrent.sh ~/Pentesting/targets/$p; done < ~/Pentesting/targets/programs.txt
+while read p; do /home/al-madjus/Pentesting/recon/recurrent.sh /home/al-madjus/Pentesting/targets/$p; done < /home/al-madjus/Pentesting/targets/programs.txt
 
 ### Do recon on the results from above ###
 TODAY=$(date +%d%m%Y)
 
 ### Run ffuf ###
-while read p; do ffuf -u $p/FUZZ -w /usr/share/wordlists/default.txt -v -mc 200 -ac; done < ~/Pentesting/targets/_results/subs-$TODAY.txt >> ~/Pentesting/targets/_results/ffuf.txt
-cat ~/Pentesting/targets/_results/ffuf.txt | grep '| URL |' | cut -c 14- | tee -a ~/Pentesting/targets/_results/ffuf_$TODAY.txt
-rm ~/Pentesting/targets/_results/ffuf.txt
+while read p; do ffuf -u $p/FUZZ -w /usr/share/wordlists/default.txt -v -mc 200 -ac; done < /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt >> /home/al-madjus/Pentesting/targets/_results/ffuf.txt
+cat /home/al-madjus/Pentesting/targets/_results/ffuf.txt | grep '| URL |' | cut -c 14- | tee -a /home/al-madjus/Pentesting/targets/_results/ffuf_$TODAY.txt
+rm /home/al-madjus/Pentesting/targets/_results/ffuf.txt
 
 ### Run nuclei with all templates ###
-nuclei -l ~/Pentesting/targets/_results/subs-$TODAY.txt -t ~/nuclei-templates/dns -t ~/nuclei-templates/generic-detections -t ~/nuclei-templates/panels -t ~/nuclei-templates/subdomain-takeovers -t ~/nuclei-templates/tokens -t ~/nuclei-templates/files -t ~/nuclei-templates/security-misconfiguration -t ~/nuclei-templates/technologies -t ~/nuclei-templates/vulnerabilities -o ~/Pentesting/targets/_results/nuclei-$TODAY.txt
+nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/dns -t /home/al-madjus/nuclei-templates/generic-detections -t /home/al-madjus/nuclei-templates/panels -t /home/al-madjus/nuclei-templates/subdomain-takeovers -t /home/al-madjus/nuclei-templates/tokens -t /home/al-madjus/nuclei-templates/files -t /home/al-madjus/nuclei-templates/security-misconfiguration -t /home/al-madjus/nuclei-templates/technologies -t /home/al-madjus/nuclei-templates/vulnerabilities -o /home/al-madjus/Pentesting/targets/_results/nuclei-$TODAY.txt
 
 ### Send email when finished ###
-cat ~/Pentesting/targets/_results/subs-$TODAY.txt | mutt -s "Recon finished!" -- $1
+cat /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt | mutt -s "Recon finished!" -- $1
