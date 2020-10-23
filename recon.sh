e0585885f082699f6bd403ac30523df325f9681d
Al-Madjus <klarsen@klarsen.net>
2020-10-23 10:57:21 +0200

    Reworked files

--- recon.sh
+++ recon.sh
@@ -2,27 +2,29 @@
 # Set PATH here since cron has very limited PATH
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
+#TODAY=$(date +%d%m%Y)
+
+### Backup files
+cat /home/al-madjus/Pentesting/targets/_results/nuclei-*.new >> /home/al-madjus/Pentesting/targets/_results/nuclei.old
+rm /home/al-madjus/Pentesting/targets/_results/nuclei-*.new
+cat /home/al-madjus/Pentesting/targets/_results/ffuf.new >> /home/al-madjus/Pentesting/targets/_results/ffuf.old
+rm /home/al-madjus/Pentesting/targets/_results/ffuf.new
+cat /home/al-madjus/Pentesting/targets/_results/subs.new >> /home/al-madjus/Pentesting/targets/_results/subs.old
+cat /home/al-madjus/Pentesting/targets/_results/ports.new >> /home/al-madjus/Pentesting/targets/_results/ports.old
+rm /home/al-madjus/Pentesting/targets/_results/subs.new
+rm /home/al-madjus/Pentesting/targets/_results/ports.new
+
+### Run recon ###
 while read p; do /home/al-madjus/Pentesting/recon/recurrent.sh /home/al-madjus/Pentesting/targets/$p; done < /home/al-madjus/Pentesting/targets/programs.txt
 
-### Do recon on the results from above ###
-TODAY=$(date +%d%m%Y)
-
 ### Run ffuf ###
-while read p; do ffuf -u $p/FUZZ -w /usr/share/wordlists/default.txt -v -mc 200 -ac; done < /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt >> /home/al-madjus/Pentesting/targets/_results/ffuf.txt
-cat /home/al-madjus/Pentesting/targets/_results/ffuf.txt | grep '| URL |' | cut -c 14- | tee -a /home/al-madjus/Pentesting/targets/_results/ffuf_$TODAY.txt
+while read p; do ffuf -u $p/FUZZ -w /usr/share/wordlists/default.txt -v -mc 200 -ac; done < /home/al-madjus/Pentesting/targets/_results/subs.new >> /home/al-madjus/Pentesting/targets/_results/ffuf.txt
+cat /home/al-madjus/Pentesting/targets/_results/ffuf.txt | grep '| URL |' | cut -c 14- | tee -a /home/al-madjus/Pentesting/targets/_results/ffuf.new
 rm /home/al-madjus/Pentesting/targets/_results/ffuf.txt
 
 ### Run nuclei with all templates ###
-nuclei --update-templates
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/dns -o /home/al-madjus/Pentesting/targets/_results/nuclei-dns-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/generic-detections -o /home/al-madjus/Pentesting/targets/_results/nuclei-generic-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/panels -o /home/al-madjus/Pentesting/targets/_results/nuclei-panels-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/subdomain-takeovers -o /home/al-madjus/Pentesting/targets/_results/nuclei-takeovers-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/tokens -o /home/al-madjus/Pentesting/targets/_results/nuclei-tokens-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/files -o /home/al-madjus/Pentesting/targets/_results/nuclei-files-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/security-misconfiguration -o /home/al-madjus/Pentesting/targets/_results/nuclei-misconf-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/technologies -o /home/al-madjus/Pentesting/targets/_results/nuclei-tech-$TODAY.txt
-nuclei -l /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt -t /home/al-madjus/nuclei-templates/vulnerabilities -o /home/al-madjus/Pentesting/targets/_results/nuclei-vulns-$TODAY.txt
+/usr/local/bin/nuclei --update-templates
+while read p; do /usr/local/bin/nuclei -l /home/al-madjus/Pentesting/targets/_results/subs.new -t /home/al-madjus/nuclei-templates/$p -o /home/al-madjus/Pentesting/targets/_results/nuclei-$p.new; done < /home/al-madjus/Pentesting/recon/templates.txt
 
 ### Send email when finished ###
-cat /home/al-madjus/Pentesting/targets/_results/subs-$TODAY.txt | mutt -s "Recon finished!" -- $1
+cat /home/al-madjus/Pentesting/targets/_results/subs.new | mutt -s "Recon finished!" -- $1
--- recurrent.sh
+++ recurrent.sh
@@ -7,8 +7,12 @@ TODAY=$(date +%d%m%Y)
 ### Check how many subs we already had ###
 before=$(cat $DIR/alive.txt|wc -l)
 
-### Backup alive subs ###
+### Backup files ###
 cp $DIR/alive.txt $DIR/alive.old
+#cat $DIR/../../_results/subs.new >> $DIR/../../_results/subs.old
+#cat $DIR/../../_results/ports.new >> $DIR/../../_results/ports.old
+#rm $DIR/../../_results/subs.new
+#rm $DIR/../../_results/ports.new
 
 ### Find which subs are alive ### 
 echo -e "Finding active subdomains on $1... "
@@ -30,10 +34,10 @@ let result=${after}-${before}
 echo ${result}
 
 ### Display all the new subs ### 
-echo "### $1 ###" >> $DIR/../../_results/subs-$TODAY.txt
+echo "### $1 ###" >> $DIR/../../_results/subs.new
 echo -e "These are the new subdomains found:"
 #echo "New subdomains: " >> ~/output-$TODAY.txt
-grep -F -x -v -f $DIR/alive.old $DIR/alive.txt | tee -a $DIR/../../_results/subs-$TODAY.txt
+grep -F -x -v -f $DIR/alive.old $DIR/alive.txt | tee -a $DIR/../../_results/subs.new
 rm $DIR/alive.old
 
 ### Remove http:// and https:// from the newest file ###
@@ -49,11 +53,11 @@ cat $DIR/ip_hosts.txt | jq '.data.answers' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9
 mv $DIR/masscan.txt $DIR/masscan.old
 
 ### Run masscan ###
-sudo masscan -iL $DIR/ip.txt -p0-79,81-442,444-1000,1001,1008,1019,1021-1034,1036,1038-1039,1041,1043-1045,1049,1068,1419,1433-1434,1522,1645-1646,1701,1718-1719,1782,1812-1813,1885,1900,2000,2002,2048-2049,2148,2222-2223,2967,3000-3001,3052,3130,3283,3306,3389,3456,3659,3703,4000,4045,4443-4444,4500,4672,5000-5001,5060-5061,5093,5351,5353,5355,5432,5500,5632,5900,6000-6001,6346,7938,8000,9000-9001,9200,9876,10000,10080,11487,16680,17185,19283,19682,20031,22986,27892,30718,31337,32768-32773,32815,33281,33354,34555,34861-34862,37444,39213,41524,44968,49152-49154,49156,49158-49159,49162-49163,49165-49166,49168,49171-49172,49179-49182,49184-49196,49199-49202,49205,49208-49211,58002,65024 --rate 1000 -oG $DIR/masscan.txt
+sudo masscan -iL $DIR/ip.txt -p0-79,81-442,444-1000,1001,1008,1019,1021-1034,1036,1038-1039,1041,1043-1045,1049,1068,1419,1433-1434,1522,1645-1646,1701,1718-1719,1782,1812-1813,1885,1900,2000,2002,2048-2049,2148,2222-2223,2967,3000-3001,3052,3130,3283,3306,3389,3456,3659,3703,4000,4045,4443-4444,4500,4672,5000-5001,5060-5061,5093,5351,5353,5355,5432,5500,5632,5900,6000-6001,6346,7938,8000,9000-9001,9200,9876,10000,10080,11487,16680,17185,19283,19682,20031,22986,27892,30718,31337,32768-32773,32815,33281,33354,34555,34861-34862,37444,39213,41524,44968,49152-49154,49156,49158-49159,49162-49163,49165-49166,49168,49171-49172,49179-49182,49184-49196,49199-49202,49205,49208-49211,58002,65024 --rate 500 -oG $DIR/masscan.txt
 
 ### Display new ports ### 
-echo "New ports: " >> $DIR/../../_results/ports-$TODAY.txt
-grep -Fxvf $DIR/masscan.old $DIR/masscan.txt | tee -a $DIR/../../_results/ports-$TODAY.txt
+echo "New ports: " >> $DIR/../../_results/ports.new
+grep -Fxvf $DIR/masscan.old $DIR/masscan.txt | tee -a $DIR/../../_results/ports.new
 
 ### Clean up ###
 rm $DIR/alive.tmp
