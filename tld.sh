aaa0cd6f91dfadf25656e513c9293ac1e86e9595
al-madjus <klarsen@klarsen.net>
2021-02-03 15:17:15 +0000

    Added support for TLD only domains

---
+++ tld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR=$2/scope
+
+# Run crtsh.sh
+crtsh.sh $1 >> $DIR/crtsh.txt
+
+# Grep domain names
+cat $DIR/crtsh.txt | awk -F "." '{print $(NF-2)"."$(NF-1)"."$NF}' > $DIR/domains.txt
+
+# Remove duplicates
+sort -uo $DIR/domains.txt $DIR/domains.txt
